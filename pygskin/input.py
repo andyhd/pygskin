b77e596d67770a116dc128f158bcd058138091d6
Andy Driver <andy@pagezero.net>
2022-09-27 15:11:15 +0100

    Refactor input handlers

--- pygskin/input.py
+++ pygskin/input.py
@@ -1,39 +1,47 @@
 from __future__ import annotations
 
 from inspect import Signature
-from types import MethodType
 from typing import Callable
 
 from pygskin.events import Event
 from pygskin.interfaces import Updatable
 from pygskin.timer import Timer
 
+# from types import MethodType
+
 
 class Input:
-    def __init__(self, handler_name: str, *args, **kwargs) -> None:
-        self.handler_name = handler_name
-        self.args = args
-        self.kwargs = kwargs
+    def __init__(self, handler: Callable, *args, **kwargs) -> None:
+        self.handler = handler
+
+        params = Signature.from_callable(handler).bind(*args, **kwargs)
+        params.apply_defaults()
+
+        self.args = params.args
+        self.kwargs = params.kwargs
+
+    def execute(self) -> None:
+        self.handler(*self.args, **self.kwargs)
 
 
-class input_handler:
-    def __init__(self, fn: Callable | None = None) -> None:
-        self.fn = fn
+# class input_handler:
+#     def __init__(self, fn: Callable | None = None) -> None:
+#         self.fn = fn
 
-    def __set_name__(self, owner_class: type, name: str) -> None:
-        if not hasattr(owner_class, "_input_handlers"):
-            setattr(owner_class, "_input_handlers", {})
-        getattr(owner_class, "_input_handlers").setdefault(name, []).append(self.fn)
+#     def __set_name__(self, owner_class: type, name: str) -> None:
+#         if not hasattr(owner_class, "_input_handlers"):
+#             setattr(owner_class, "_input_handlers", {})
+#         getattr(owner_class, "_input_handlers").setdefault(name, []).append(self.fn)
 
-    def __get__(self, instance, _=None) -> input_handler | MethodType:
-        if instance and callable(self.fn):
-            return MethodType(self.fn, instance)
-        return self
+#     def __get__(self, instance, _=None) -> input_handler | MethodType:
+#         if instance and callable(self.fn):
+#             return MethodType(self.fn, instance)
+#         return self
 
-    def __call__(self, fn: Callable) -> input_handler | None:
-        if self.fn is None and callable(fn):
-            self.fn = fn
-            return self
+#     def __call__(self, fn: Callable) -> input_handler | None:
+#         if self.fn is None and callable(fn):
+#             self.fn = fn
+#             return self
 
 
 class InputHandler(Updatable):
@@ -42,7 +50,7 @@ class InputHandler(Updatable):
     def get_input_for_event(self, event: Event) -> Input | None:
         input = self.event_map.get(event)
         if input is None and isinstance(event, Timer):
-            input = Input("timer", event)
+            input = Input(self.timer, event)
         if isinstance(input, str):
             input = Input(input)
         return input
@@ -54,21 +62,9 @@ class InputHandler(Updatable):
             if input is not None
         ]
 
-    def handle_input(self, input: Input) -> None:
-        try:
-            handler = getattr(self, input.handler_name)
-        except AttributeError:
-            return None
-
-        params = Signature.from_callable(handler).bind(*input.args, **input.kwargs)
-        params.apply_defaults()
-
-        return handler(*params.args, **params.kwargs)
-
     def update(self, _: float) -> None:
         for input in self.get_inputs():
-            self.handle_input(input)
+            input.execute()
 
-    @input_handler
     def timer(self, timer: Timer) -> None:
         timer.finish()
