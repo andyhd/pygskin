e299954d6a694ad569e5c6ebd8282ccdc04f95e2
Andy Driver <andy@pagezero.net>
2025-01-16 00:21:39 +0000

    Simplify spritesheet
    * Refactor class to function wrapping new grid function

--- pygskin/rect.py
+++ pygskin/rect.py
@@ -1,6 +1,9 @@
+from collections.abc import Callable
 from collections.abc import Sequence
+from typing import overload
 
 from pygame import Rect
+from pygame.typing import IntPoint
 
 RECT_ATTRS = """
     x y
@@ -42,3 +45,43 @@ def add_padding(
             )
 
     raise ValueError("Invalid padding")
+
+
+def grid(
+    rect: Rect,
+    rows: int = 1,
+    columns: int = 1,
+    names: dict[str, IntPoint] | None = None,
+) -> Callable[..., Rect]:
+    """Return a function to access cells in a grid of rectangles.
+
+    >>> rect = Rect(0, 0, 100, 50)
+    >>> get_cell = grid(rect, rows=2, columns=4, names={"foo": (3, 1)})
+    >>> get_cell(1, 1)
+    Rect(25, 25, 25, 25)
+    >>> get_cell("foo")
+    Rect(75, 25, 25, 25)
+    """
+
+    cellw, cellh = rect.width // columns, rect.height // rows
+
+    @overload
+    def get_cell(key: str) -> Rect: ...
+
+    @overload
+    def get_cell(column: int, row: int) -> Rect: ...
+
+    def get_cell(*args, **kwargs) -> Rect:
+        match args:
+            case (str(key),) if names and key in names:
+                coord = names[key]
+                column, row = coord[0], coord[1]
+            case (int(column), int(row)):
+                ...
+            case _:
+                raise KeyError(*args)
+        if not (0 <= column < columns and 0 <= row < rows):
+            raise KeyError(*args)
+        return Rect(column * cellw, row * cellh, cellw, cellh)
+
+    return get_cell
--- pygskin/spritesheet.py
+++ pygskin/spritesheet.py
@@ -1,56 +1,32 @@
-from dataclasses import dataclass
+from collections.abc import Callable
+from functools import cache
 
-import pygame
+from pygame import Surface
+
+from pygskin.rect import grid
 
 
-@dataclass
-class Spritesheet:
-    """A class to represent a spritesheet.
+def spritesheet(image: Surface, *args, **kwargs) -> Callable[..., Surface]:
+    """Return a function to access cells in a spritesheet.
 
-    >>> _ = pygame.init()
-    >>> _ = pygame.display.set_mode((100, 50))
-    >>> image = pygame.Surface((100, 50))
-    >>> colors = "red yellow green brown blue pink black white".split()
-    >>> coords = [(x, y) for y in range(2) for x in range(4)]
-    >>> name_map = dict(zip(colors, coords))
-    >>> for i, (color, (x, y)) in enumerate(name_map.items()):
-    ...     pygame.draw.rect(image, colors[i], (x * 25, y * 25, 25, 25))
-    Rect(0, 0, 25, 25)
+    >>> image = Surface((20, 10))
+    >>> colors = {"r": [255, 0, 0, 255], "g": [0, 255, 0, 255], "b": [0, 0, 255, 255]}
+    >>> coords = [(x, 0) for x in range(3)]
+    >>> names = dict(zip(colors.keys(), coords))
+    >>> for i, (color, (x, y)) in enumerate(names.items()):
+    ...     image.fill(colors[color], (x * 5, y * 5, 5, 5))
+    Rect(0, 0, 5, 5)
     ...
-    >>> sheet = Spritesheet(image, rows=2, columns=4, name_map=name_map)
-    >>> all(
-    ...     all(
-    ...         sheet[color].get_at((x, y)) == pygame.Color(color)
-    ...         for x in range(25) for y in range(25)
-    ...     )
-    ...     for color in colors
-    ... )
+    >>> sheet = spritesheet(image, rows=1, columns=3, names=names)
+    >>> all(list(sheet(name).get_at((0, 0))) == color for name, color in colors.items())
     True
     """
 
-    image: pygame.Surface
-    rows: int = 1
-    columns: int = 1
-    name_map: dict[str, tuple[int, int]] | None = None
+    get_cell = grid(image.get_rect(), *args, **kwargs)
 
-    def __post_init__(self) -> None:
-        self.grid_size = pygame.Vector2(self.columns, self.rows)
+    @cache
+    def get_subsurface(*args, **kwargs) -> Surface:
+        return image.subsurface(get_cell(*args, **kwargs))
 
-    @property
-    def cell_size(self) -> tuple[int, int]:
-        if not hasattr(self, "_cell_size"):
-            width, height = self.image.get_size()
-            self._cell_size = (width // self.columns, height // self.rows)
-        return self._cell_size
+    return get_subsurface
 
-    def __getitem__(self, key: str | tuple[int, int]) -> pygame.Surface:
-        match key:
-            case str(skey) if self.name_map and (coord := self.name_map[skey]):
-                return self[coord]
-
-            case tuple(t) if (0 <= t[0] < self.columns and 0 <= t[1] < self.rows):
-                cell_w, cell_h = self.cell_size
-                pos = (t[0] * cell_w, t[1] * cell_h)
-                return self.image.subsurface((pos, self.cell_size))
-
-        raise KeyError
